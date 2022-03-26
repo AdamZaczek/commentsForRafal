a499edfdf013218655231e29f36223064eb549b8
adamzaczek <adamzaczekul@gmail.com>
2022-03-26 11:25:19 +0100

    Changes for Rafal

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import "./App.css";
 import { useState } from "react";
 // import Comment from "./Comment";
 
-const comments = [
-  {
-    text: "Taki se",
-    author: {
-      name: "Rafał",
-      avatar:
-        "https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1",
-    },
+// const comments = [
+//   {
+//     text: "Taki se",
+//     author: {
+//       name: "Rafał",
+//       avatar:
+//         "https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1",
+//     },
+//   },
+// ];
+
+const comment = {
+  text: "Taki se",
+  author: {
+    name: "Rafał",
+    avatar:
+      "https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1",
   },
-];
+};
 
 const Comment = (props) => {
   const [likes, setLikes] = useState(0);
@@ -41,7 +50,8 @@ const Comment = (props) => {
 };
 
 function App() {
-  return comments.map((x) => <Comment text={x.text} author={x.author} />);
+  return <Comment {...comment} />;
+  // return comments.map((x) => <Comment text={x.text} author={x.author} />);
 }
 
 export default App;
