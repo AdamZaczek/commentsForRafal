55b36ba7136f376ec5e1419ba2ac109da9705540
adamzaczek <adamzaczekul@gmail.com>
2022-03-26 11:19:21 +0100

    Changes for Rafal

--- src/Comment.js
+++ src/Comment.js
@@ -1,16 +1,4 @@
-import "./App.css";
 import { useState } from "react";
-// import Comment from "./Comment";
-
-const comments = [
-  {
-    author: "Rafał",
-    text: "Taki se",
-    avatar:
-      "https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1",
-  },
-];
-
 
 const Comment = (props) => {
   const [likes, setLikes] = useState(0);
@@ -37,8 +25,4 @@ const Comment = (props) => {
   );
 };
 
-function App() {
-  return comments.map((x) => <Comment text={x.text} author={x.author} />);
-}
-
-export default App;
+export default Comment;
